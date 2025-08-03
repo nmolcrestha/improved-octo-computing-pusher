16e19b13622e482bb7ba83acfd5805939a93daee
Anmol Shrestha <nmol.crestha@gmail.com>
2025-08-03 21:48:31 +0545

    Pusher configured in the project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
-import './bootstrap';
+import "./bootstrap";
+import Echo from "laravel-echo";
+import Pusher from "pusher-js";
+
+window.Pusher = Pusher;
+
+window.Echo = new Echo({
+    broadcaster: "pusher",
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    forceTLS: true,
+});
+
+window.Echo.channel("my-channel").listen(".my-event", (e) => {
+    console.log("Received event:", e.message);
+});
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Events\MessageSent;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/", function () {
+    return view("welcome");
+});
+
+Route::get("/send", function () {
+    broadcast(new MessageSent("Hello from Laravel!"));
+    return "Message broadcasted!";
 });
